a43119cd966fe03a80f9630126613718fa61c849
VikinG <102913739+VikinG-11111@users.noreply.github.com>
2022-10-08 10:18:47 +0545

    Update generating_qrcode_barcode.py

--- generating_qrcode_barcode.py
+++ generating_qrcode_barcode.py
@@ -1,9 +1,90 @@
 import pyqrcode
+import shutil
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
-again = True
-while again == True:
+
+def lines():
+    terminal_width = shutil.get_terminal_size().columns
+    to_print = "_"
+    display_terminal = to_print * terminal_width
+    print(display_terminal)
+
+
+def qrcode():
+    data_to_encode = input("Enter the data you want to get encoded into QR code: ")
+    qr_code = pyqrcode.create(data_to_encode)
+    first_image_name = input("Enter image name: ")
+    last_image_name = ".png"
+    image_name = first_image_name + last_image_name
+    qr_code.svg(image_name, scale=8)
+    print()
+    while True:
+        asking_user = input(
+            "Do you want to continue generating another image?(Enter Y for 'Yes', N for 'No': "
+        )
+        if asking_user == "Y" or asking_user == "y":
+
+            lines()
+            break
+        elif asking_user == "N" or asking_user == "n":
+
+            print()
+            print("Thank You for using this program. ")
+            print("- Made by VikinG")
+            lines()
+            quit()
+        else:
+            print("Invalid Character, enter again. ")
+            lines()
+            print()
+
+
+def barcode():
+    while True:
+        print()
+        number = input("Enter 12 digits number you want to get encoded into Bar code: ")
+        number_length = len(number)
+        if number_length == 12:
+            bar_code = EAN13(number, writer=ImageWriter())
+            image_name = input("Enter image name: ")
+            bar_code.save(image_name)
+            print()
+            while True:
+                asking_user = input(
+                    "Do you want to continue generating another image?(Enter Y for 'Yes', N for 'No': "
+                )
+                if asking_user == "Y" or asking_user == "y":
+
+                    lines()
+                    break
+                elif asking_user == "N" or asking_user == "n":
+
+                    print()
+                    print("Thank You for using this program. ")
+                    print("- Made by VikinG ")
+                    lines()
+                    quit()
+                else:
+                    print("Invalid Character, enter again. ")
+                    lines()
+                    print()
+            break
+        else:
+            print("The input must have 12 digits. ")
+            lines()
+            print()
+
+
+def exit_program():
+    print("Thank You for using this program. ")
+    print("- Made by VikinG ")
+    print()
+    lines()
+    quit()
+
+
+while True:
     print()
     print("This program generates QR codes and Bar codes in PNG format. ")
     print("1. QR code ")
@@ -11,98 +92,14 @@ while again == True:
     print("3. Quit ")
     print()
     choice = input("Choose which one to generate: ")
-    print(
-        "________________________________________________________________________________________________________________"
-    )
+    lines()
     print()
     if choice == "1":
-        data_to_encode = input("Enter the data you want to get encoded into QR code: ")
-        qr_code = pyqrcode.create(data_to_encode)
-        first_image_name = input("Enter image name: ")
-        last_image_name = ".png"
-        image_name = first_image_name + last_image_name
-        qr_code.svg(image_name, scale=8)
-        print()
-        while again == True:
-            asking_user = input(
-                "Do you want to continue generating another image?(Enter Y for 'Yes', N for 'No': "
-            )
-            if asking_user == "Y" or asking_user == "y":
-                again = True
-                print(
-                    "________________________________________________________________________________________________________________"
-                )
-                break
-            elif asking_user == "N" or asking_user == "n":
-                again = False
-                print()
-                print("Thank You for using this program. ")
-                print("- Made by VikinG")
-                print(
-                    "________________________________________________________________________________________________________________"
-                )
-                quit()
-            else:
-                print("Invalid Character, enter again. ")
-                print(
-                    "________________________________________________________________________________________________________________"
-                )
-                print()
+        qrcode()
     elif choice == "2":
-        while again == True:
-            print()
-            number = input(
-                "Enter 12 digits number you want to get encoded into Bar code: "
-            )
-            number_length = len(number)
-            if number_length == 12:
-                bar_code = EAN13(number, writer=ImageWriter())
-                image_name = input("Enter image name: ")
-                bar_code.save(image_name)
-                print()
-                while again == True:
-                    asking_user = input(
-                        "Do you want to continue generating another image?(Enter Y for 'Yes', N for 'No': "
-                    )
-                    if asking_user == "Y" or asking_user == "y":
-                        again = True
-                        print(
-                            "________________________________________________________________________________________________________________"
-                        )
-                        break
-                    elif asking_user == "N" or asking_user == "n":
-                        again = False
-                        print()
-                        print("Thank You for using this program. ")
-                        print("- Made by VikinG ")
-                        print(
-                            "________________________________________________________________________________________________________________"
-                        )
-                        quit()
-                    else:
-                        print("Invalid Character, enter again. ")
-                        print(
-                            "________________________________________________________________________________________________________________"
-                        )
-                        print()
-            else:
-                print("The input must have 12 digits. ")
-                print(
-                    "________________________________________________________________________________________________________________"
-                )
-                print()
-            break
-
+        barcode()
     elif choice == "3":
-        print("Thank You for using this program. ")
-        print("- Made by VikinG ")
-        print()
-        print(
-            "________________________________________________________________________________________________________________"
-        )
-        quit()
+        exit_program()
     else:
         print("Invalid input, please input again. ")
-        print(
-            "________________________________________________________________________________________________________________"
-        )
+        lines()
